1074eb805385bbf0bcb25e34ef6250086a776d7b
Kenji Sousa <kdasnerd@gmail.com>
2025-11-15 18:06:56 -0300

    Adicionada validação de login por cookies, possibilitando logar pelo app web

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -2,6 +2,7 @@ import { LoginRequest } from "#interfaces/authInterfaces.js";
 import express from "express";
 import * as LoginService from "#service/loginService.js";
 import jwt from "jsonwebtoken";
+import { authenticateJWT } from "#auth/authenticateJWT.js";
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET ?? "";
@@ -12,7 +13,24 @@ router.post("", async (req: LoginRequest, res) => {
   const payload = await LoginService.login(idToken, deviceId);
   const token = jwt.sign(payload, JWT_SECRET, {});
 
+  // mobile
+  if (deviceId) {
   return res.json({ token });
+  }
+
+  // web
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    })
+    .send();
+});
+
+router.get("/validate", authenticateJWT, (_req, res) => {
+  console.log("cookies: " + JSON.stringify(_req.cookies));
+  res.status(200).send();
 });
 
 export default router;
