9fd70552faf5ef0dab00ddfbf0d10fdf54069f41
Kenji Sousa <kdasnerd@gmail.com>
2025-11-15 17:55:28 -0300

    Adicionada validação de login por cookies, possibilitando logar pelo app web

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import { authenticateJWT } from "#auth/authenticateJWT.js";
 import { configureDB } from "#infrastructure/configureDB.js";
@@ -10,6 +11,7 @@ import eventoController from "#controller/eventoController.js";
 import presencaController from "#controller/presencaController.js";
 import presencaLinkController from "#controller/presencaLinkController.js";
 import errorHandler from "#error/errorHandler.js";
+import cookieParser from "cookie-parser";
 
 await configureDB();
 
@@ -23,6 +25,16 @@ router.use("/presenca", authenticateJWT, presencaController);
 router.use("/presenca-link", authenticateJWT, presencaLinkController);
 
 const app = express();
+
+app.use(
+  cors({
+    // Para testes com o NextJS
+    origin: "http://localhost:3001",
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
 
 app.use("/api", bodyParser.json(), router, errorHandler);
 
